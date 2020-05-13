261e026f06bfd72169e3c08e6115c5a4177b6ea6
Emeruche Cole <emeruchecole9@gmail.com>
2020-05-13 22:44:34 +0100

    fixed referreal issue

--- users/models.py
+++ users/models.py
@@ -2,12 +2,17 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from ponzi.settings import AUTH_USER_MODEL
 from rest_framework import serializers
+import random
+import string
+
+def get_referral_code(stringLength=8):
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(stringLength))
 
 # Create your models here.
 class User(AbstractUser):
-    email = models.EmailField(max_length=254, unique=True)
-    referrer = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True)
-    # referrer_id = models.IntegerField(default=0, blank=True,   null=True)
+    email = models.EmailField(max_length=254, unique=True)referral_code = models.CharField(max_length=100, default=get_referral_code(), unique=True) #code for referring others
+    referrer_code = models.CharField(max_length=100, null=True, blank=True) #code of the person who referred the user
     wallet_address = models.CharField(max_length=200, blank=True, null=True, unique=True)
     to_wallet = models.CharField(max_length=200, blank=True, null=True, unique=True)
     balance = models.DecimalField(max_digits=50, decimal_places=8, default=0.00000000)
@@ -15,25 +20,26 @@ class User(AbstractUser):
     phone = models.PositiveIntegerField(null=True, blank=True)
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username', 'referrer', 'level', 'first_name', 'last_name']
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
 
     def __str__(self):
         return self.username
 
     def get_referrer(self):
         try:
-            user = User.objects.get(id = self.referrer.id)
-            referrer = {
+            user = User.objects.get(referral_code = self.referrer_code)
+            upline = {
                 'id': user.id,
                 'username': user.username,
-                'level': user.level
+                'level': user.level,
+                'referral_code': user.referral_code
             }
         except:
-            referrer = 'null'
-        return referrer
+            upline = 'null'
+        return upline
 
     def get_downlines(self):
-        downlines = User.objects.filter(referrer = self)
+        downlines = User.objects.filter(referrer_code = self.referral_code)
         data = []
         for user in downlines:
             data.append({
--- users/serializers.py
+++ users/serializers.py
@@ -4,7 +4,7 @@ from ponzi.settings import AUTH_USER_MODEL
 from djoser.serializers import UserCreateSerializer as BaseUserRegistrationSerializer
 
 class UserSerializer(serializers.ModelSerializer):
-    referrer = serializers.SerializerMethodField()
+    upline = serializers.SerializerMethodField()
     downlines = serializers.SerializerMethodField()
     class Meta:
         model = User
@@ -13,28 +13,10 @@ class UserSerializer(serializers.ModelSerializer):
         # fields = ['email', 'username', 'referrer_id']
 
 
-    def get_referrer(self, obj):
-        # try:
-        #     user = User.objects.get(id = obj.referrer.id)
-        #     referrer = {
-        #         'id': user.id,
-        #         'username': user.username,
-        #         'level': user.level
-        #     }
-        # except (User.DoesNotExist, AttributeError):
-        #     referrer = None
+    def get_upline(self, obj):
         return obj.get_referrer()
 
     def get_downlines(self, obj):
-        # users = User.objects.filter(referrer = obj)
-        # data = []
-        # for user in users:
-        #     data.append({
-        #         'id': user.id,
-        #         'username': user.username,
-        #         'level': user.level,
-        #         'downlines': user.get_downlines()
-        #     })
         return obj.get_downlines()
             
 
